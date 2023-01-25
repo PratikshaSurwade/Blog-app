4882602023460e9aeb0cbf054deeba425f7926f6
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2023-01-25 13:19:36 +0530

    Rename

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Route,Routes } from "react-router-dom";
 import Navbar from "./component/NavbarPage.js";
 import Home from "./component/Homepage/Home.js";
 import ScrollToTop from "./Scrolltop.js"
-import Articlebyid from "./component/page3/Article.js";
+import Articlebyid from "./component/page3/ArticlePage.js";
 import Categorypage from "./component/page2/Page2.js";
 
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
